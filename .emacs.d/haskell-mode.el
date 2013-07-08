3e264f17db9151d81ea50dfef849b2f5b28eb30f
Vincent Savard <savard.vincent at gmail dot com>
2013-07-07 22:01:51 -0400

    Changing the path to Haskell Mode

--- .emacs.d/haskell-mode.el
+++ .emacs.d/haskell-mode.el
@@ -1,3 +1,3 @@
-(load "/usr/share/emacs/site-lisp/haskell-mode/haskell-site-file")
+(load "/usr/share/emacs/site-lisp/haskell-mode/haskell-mode.el")
 (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
 (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
